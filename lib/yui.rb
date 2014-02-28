61ec669dca55fd0550c9dc42c36e6152a33a0fec
Võ Anh Duy <voanhduy1512@live.com>
2014-03-01 03:54:54 +0700

    first run

--- lib/yui.rb
+++ lib/yui.rb
@@ -1,4 +1,2 @@
 require "yui/version"
 require "yui/handlers/yui"
-module Yui
-end
--- lib/yui/handlers/yui.rb
+++ lib/yui/handlers/yui.rb
@@ -1,30 +1,22 @@
 require "lita"
 
-module Yui
+module Lita
   module Handlers
     class YuiHandler < Handler
       # Lita HTTPRoute for Slack: Outgoing WebHook integration
       route /^yui\shelp\s+(.+)/, :help
+      route /^yui\swant\sto\s+(.+), :want_to
 
       # Class method called by Lita for handler configuration
       def self.default_config(default)
       end
 
-      def help(message)
-        binding.pry
-        # log.debug "SlackHandler::receive webhook_token:#{req['token']} team_domain:#{req['team_domain']}"
-        # log.debug "SlackHandler::receive user id:#{req['user_id']} name:#{req['user_name']}"
-        # log.debug "SlackHandler::receive room id:#{req['channel_id']} name:#{req['channel_name']}"
-        # log.debug "SlackHandler::receive message text size #{req['text'].size} byte(s)"
-        # user = User.create(req['user_id'], name: req['user_name'], mention_name: req['user_name'])
-        # # Register channel using Lita::User class
-        # room = User.create(req['channel_id'], name: req['channel_name'])
-        # source = Source.new(user: user, room: room.id)
-        # message = Message.new(robot, req['text'], source)
-        # # Route the message to the adapter
-        # log.info 'SlackHandler::receive routing message to the adapter'
-        # robot.receive message
-        # log.debug 'SlackHandler::receive ending'
+      def help(response)
+        response.reply "How can I help you?"
+      end
+
+      def want_to(response)
+        response.reply "I know that you want to #{response.matches} but Mr.Duy doesn't allow me to do it for you now. Sorry"
       end
 
       #
